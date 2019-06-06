b7eb92599e0081e5bab261435b4d0c2566721af0
timolchondra <tim.olchondra@gmail.com>
2019-06-05 17:55:32 -0700

    added houses, apartments, public/private elementary/middle/high schools

--- databaseGUI.py
+++ databaseGUI.py
@@ -33,6 +33,7 @@ def ethnicWindow():
     for x in myresult:
         ethnicGroupList.append(x)
 
+    myresult.clear()
     rows = 1
     for x in ethnicGroupList:
         group = x[0]
@@ -94,10 +95,270 @@ def allVillagesWindow():
 
 
 class VillageWindow:
+    villageName = ""
+    labels = []
+    buttons = []
+
     def __init__(self,master,villageName):
        villageWindow = Toplevel(master)
+       self.villageName = villageName
        self.titleLabel = Label(villageWindow, text=villageName + " Village", height=3)
-       self.titleLabel.pack()
+       self.titleLabel.grid(row=0,column=0)
+       #bottomFrame = Frame(villageWindow)
+       #bottomFrame.pack(side=BOTTOM)
+       bottomFrame = Frame(villageWindow)
+       bottomFrame.grid(row=2)
+
+       var = StringVar()
+       houseButton = Radiobutton(villageWindow, indicatoron =0, text="Houses", variable=var, value="House", command=lambda: self.displayData(villageWindow,"House" ))
+       houseButton.grid(row=1,column=0)
+
+       apartmentButton = Radiobutton(villageWindow, indicatoron =0, text="Apartments", variable=var, value="Apartment", command = lambda: self.displayData(villageWindow, "Apartment"))
+       apartmentButton.grid(row=1,column=1)
+
+       publicSchoolButton = Radiobutton(villageWindow, indicatoron =0, text="Public Schools", variable=var, value="PublicSchool", command = lambda: self.displayData(villageWindow, "PublicSchool"))
+       publicSchoolButton.grid(row=1,column=2)
+
+       privateSchoolButton = Radiobutton(villageWindow, indicatoron =0, text="Private Schools", variable=var, value="PrivateSchool", command = lambda: self.displayData(villageWindow, "PrivateSchool"))
+       privateSchoolButton.grid(row=1,column=3)
+
+       collegeButton = Radiobutton(villageWindow, indicatoron =0, text="Colleges", variable=var, value="College", command = lambda: self.displayData(villageWindow, "College"))
+       collegeButton.grid(row=1,column=4)
+
+       restaurantButton = Radiobutton(villageWindow, indicatoron =0, text="Restaurants", variable=var, value="Restaurant", command = lambda: self.displayData(villageWindow, "Restaurant"))
+       restaurantButton.grid(row=1,column=5)
+
+       mallButton = Radiobutton(villageWindow, indicatoron =0, text="Malls", variable=var, value="Mall", command = lambda: self.displayData(villageWindow, "Mall"))
+       mallButton.grid(row=1,column=6)
+
+       movieTheaterButton = Radiobutton(villageWindow, indicatoron =0, text="Movie Theater", variable=var, value="Movie", command = lambda: self.displayData(villageWindow, "MovieTheater"))
+       movieTheaterButton.grid(row=1, column=7)
+
+       beachButton= Radiobutton(villageWindow, indicatoron =0, text="Beaches", variable=var, value="Beach", command=lambda: self.displayData(villageWindow, "Beach"))
+       beachButton.grid(row=1, column=8)
+
+       groceryButton = Radiobutton(villageWindow, indicatoron =0, text="Grocery Stores", variable=var, value="Grocery", command = lambda: self.displayData(villageWindow, "Grocery"))
+       groceryButton.grid(row=1, column=8)
+
+       churchButton = Radiobutton(villageWindow, indicatoron =0, text="Churches", variable=var, value="Church", command=lambda: self.displayData(villageWindow, "Church"))
+       churchButton.grid(row=1, column=9)
+
+    def displayData(self, master, buttonPressed):
+        #destroy the labels when another button is pressed
+        for x in self.labels:
+            x.destroy()
+        #destroy all buttons created
+        for x in self.buttons:
+            x.destroy()
+
+        if buttonPressed == "House":
+            mycursor.execute("SELECT House_Description, Address, Cost, NumOfBedrooms, NumOfBathrooms, SquareAcres FROM %s WHERE VillageName = '%s' ORDER BY COST"%(buttonPressed, self.villageName))
+            myresult = mycursor.fetchall()
+
+            label1 = LabelFrame(master, text="House Description")
+            label1.grid(row=2,column=0)
+            self.labels.append(label1)
+
+            label2 = LabelFrame(master, text="Address")
+            label2.grid(row=2,column=1)
+            self.labels.append(label2)
+
+            label3 = LabelFrame(master, text="Cost $")
+            label3.grid(row=2, column=2)
+            self.labels.append(label3)
+
+            label4 = LabelFrame(master, text="Number of Bedrooms")
+            label4.grid(row=2, column=3)
+            self.labels.append(label4)
+
+            label5 = LabelFrame(master, text="Number of Bathrooms")
+            label5.grid(row=2, column=4)
+            self.labels.append(label5)
+
+            label6 = LabelFrame(master, text="SquareAcres (sqft)")
+            label6.grid(row=2, column=5)
+            self.labels.append(label6)
+
+            row = 2
+            for x in myresult:
+                column=0
+                row = row + 1
+                for y in x:
+                    if column == 0:
+                        data = Label(label1, text=str(y))
+                    if column == 1:
+                        data = Label(label2, text=str(y))
+                    if column == 2:
+                        data = Label(label3, text=str(y))
+                    if column == 3:
+                        data = Label(label4, text=str(y))
+                    if column == 4:
+                        data = Label(label5, text=str(y))
+                    if column == 5:
+                        data = Label(label6, text=str(y))
+
+
+                    data.grid(row=row, column=column)
+                    self.labels.append(data)
+                    column = column + 1
+            myresult.clear()
+
+        if buttonPressed == "Apartment":
+            mycursor.execute("SELECT MLSNumber, Address, AptUnit, CostOfRent, NumOfBedrooms, NumOfBathrooms, SizeInSqft FROM Apartment WHERE VillageName = '%s' ORDER BY CostOfRent"%self.villageName)
+
+            myresult = mycursor.fetchall()
+
+            label1 = LabelFrame(master, text="MLS")
+            label1.grid(row=2,column=0)
+            self.labels.append(label1)
+
+            label2 = LabelFrame(master, text="Address")
+            label2.grid(row=2,column=1)
+            self.labels.append(label2)
+
+            label3 = LabelFrame(master, text="AptUnit")
+            label3.grid(row=2, column=2)
+            self.labels.append(label3)
+
+            label4 = LabelFrame(master, text="CostOfRent")
+            label4.grid(row=2, column=3)
+            self.labels.append(label4)
+
+            label5 = LabelFrame(master, text="Number of Bedrooms")
+            label5.grid(row=2, column=4)
+            self.labels.append(label5)
+
+            label6 = LabelFrame(master, text="Number of Bathrooms")
+            label6.grid(row=2, column=5)
+            self.labels.append(label6)
+
+            label7 = LabelFrame(master, text="Sqft")
+            label7.grid(row=2, column=6)
+            self.labels.append(label7)
+
+            row = 2
+            for x in myresult:
+                column=0
+                row = row + 1
+                for y in x:
+                    if column == 0:
+                        data = Label(label1, text=str(y))
+                    if column == 1:
+                        data = Label(label2, text=str(y))
+                    if column == 2:
+                        data = Label(label3, text=str(y))
+                    if column == 3:
+                        data = Label(label4, text=str(y))
+                    if column == 4:
+                        data = Label(label5, text=str(y))
+                    if column == 5:
+                        data = Label(label6, text=str(y))
+                    if column == 6:
+                        data = Label(label7, text=str(y))
+
+                    data.grid(row=row, column=column)
+                    self.labels.append(data)
+                    column = column + 1
+            myresult.clear()
+
+        if buttonPressed == "PrivateSchool" or buttonPressed == "PublicSchool":
+            var = StringVar()
+            elementaryButton = Radiobutton(master, indicatoron=0, text="Elementary Schools", variable=var, value="ElementarySchool", command = lambda: self.displaySchools(master, buttonPressed, "ElementarySchool") )
+            elementaryButton.grid(row=2,column=0)
+            self.buttons.append(elementaryButton)
+
+            middleButton= Radiobutton(master, indicatoron=0, text="Middle Schools", variable=var, value="MiddleSchool", command = lambda: self.displaySchools(master, buttonPressed, "MiddleSchool"))
+            middleButton.grid(row=2,column=1)
+            self.buttons.append(middleButton)
+
+            highButton = Radiobutton(master, indicatoron=0, text="High Schools", variable=var, value="HighSchool", command = lambda: self.displaySchools(master, buttonPressed, "HighSchool"))
+            highButton.grid(row=2,column=2)
+            self.buttons.append(highButton)
+
+        #if buttonPressed == "College":
+
+    def displaySchools(self, master, publicprivate ,gradeLevel):
+        for x in self.labels:
+            x.destroy()
+
+        if publicprivate == "PublicSchool":
+            mycursor.execute("SELECT SchoolName, PhoneNumber, Mascot FROM %s join PublicSchool using(PublicID) WHERE VillageName = '%s' AND PrivateID IS NULL"%(gradeLevel,self.villageName))
+            myresult = mycursor.fetchall()
+
+            label1 = LabelFrame(master, text="SchoolName")
+            label1.grid(row=3,column=0)
+            self.labels.append(label1)
+
+            label2 = LabelFrame(master, text="Phone Number")
+            label2.grid(row=3,column=1)
+            self.labels.append(label2)
+
+            label3 = LabelFrame(master, text="Mascot")
+            label3.grid(row=3, column=2)
+            self.labels.append(label3)
+
+            row = 3
+            for x in myresult:
+                column=0
+                row = row + 1
+                for y in x:
+                    if column == 0:
+                        data = Label(label1, text=str(y))
+                    if column == 1:
+                        data = Label(label2, text=str(y))
+                    if column == 2:
+                        data = Label(label3, text=str(y))
+
+                    data.grid(row=row, column=column)
+                    self.labels.append(data)
+                    column = column + 1
+            myresult.clear()
+
+        if publicprivate == "PrivateSchool":
+            mycursor.execute("SELECT SchoolName, PhoneNumber, Mascot, Denomination, Tuition FROM %s join PrivateSchool using(PrivateID) WHERE VillageName = '%s' "%(gradeLevel,self.villageName))
+            myresult = mycursor.fetchall()
+
+            label1 = LabelFrame(master, text="School Name")
+            label1.grid(row=3,column=0)
+            self.labels.append(label1)
+
+            label2 = LabelFrame(master, text="Phone Number")
+            label2.grid(row=3,column=1)
+            self.labels.append(label2)
+
+            label3 = LabelFrame(master, text="Mascot")
+            label3.grid(row=3, column=2)
+            self.labels.append(label3)
+
+            label4 = LabelFrame(master, text="Denomination")
+            label4.grid(row=3, column=3)
+            self.labels.append(label4)
+
+            label5 = LabelFrame(master, text="Tuition Quarterly($)")
+            label5.grid(row=3, column=4)
+            self.labels.append(label5)
+
+            row = 3
+            for x in myresult:
+                column=0
+                row = row + 1
+                for y in x:
+                    if column == 0:
+                        data = Label(label1, text=str(y))
+                    if column == 1:
+                        data = Label(label2, text=str(y))
+                    if column == 2:
+                        data = Label(label3, text=str(y))
+                    if column == 3:
+                        data = Label(label4, text=str(y))
+                    if column == 4:
+                        data = Label(label5, text=str(y))
+
+                    data.grid(row=row, column=column)
+                    self.labels.append(data)
+                    column = column + 1
+            myresult.clear()
+
 
 class MainWindow:
     def __init__(self, master):
